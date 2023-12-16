e64493a820af3b2c35f26b7f68e905e8e145cf99
Melaku Girma <jininuone@gmail.com>
2023-12-16 03:00:57 +0100

    controlled search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Search from './components/search';
 
-function App() {
+
+const App = () => {
+  const [word, setWord] = useState('')
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  }
+  
   return (
-    <div className="App">
-        <h1>Image Gallery</h1>
+    <div>
+      <Header title='Images Gallery' />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
