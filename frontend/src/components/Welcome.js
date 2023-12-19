3f254d05576bcac3fbf746acbd673461905b2c22
Melaku Girma <jininuone@gmail.com>
2023-12-19 19:40:09 +0100

    initial api and python virtual environment

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -9,7 +9,7 @@ const Welcome = () => (
       Inoreder to start enter any search term in the input field.
     </p>
     <p>
-      <Button bsStyle="primary" href="https://unsplash.com" target="_blank">
+      <Button bsstyle="primary" href="https://unsplash.com" target="_blank">
         Learn more
       </Button>
     </p>
